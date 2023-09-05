9e77528199c0ffdfd424358f071a456289d831d1
Mahedee Hasan <mahedee007@gmail.com>
2023-09-06 02:05:00 +0600

    final commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
+    screens:{
+      sm: '480px',
+      md:'768px',
+      lg:'976px',
+      xxl:'1440px'
+    },
     extend: {
       colors:{
         brightRed: 'hsl(12, 88%, 59%)',
